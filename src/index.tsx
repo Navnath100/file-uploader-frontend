60e815319a4ef59c39dc6717241577edc848121d
navnath <navnath@werqlabs.com>
2023-03-26 17:12:07 +0530

    added env config

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const CLIENT_ID: any = process.env.CLIENT_ID;
+const CLIENT_ID: any = process.env.REACT_APP_CLIENT_ID;
+console.log("CLIENT_ID : ", CLIENT_ID);
+
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={CLIENT_ID}>
